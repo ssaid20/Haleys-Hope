b8c842a7e023242577ec8eb45bb856ce130618cd
David Campbell <tryggus20@gmail.com>
2023-11-11 08:32:31 -0600

    finished get started post

--- server/routes/gort.router.js
+++ server/routes/gort.router.js
@@ -6,9 +6,29 @@ const {
 } = require("../modules/authentication-middleware");
 
 //Routes for GORT-5 test
+                             //GET
+router.get("/:studentId", async (req, res) => {
+    try {
+      const studentId = req.params.studentId; // TODO: may just be id from front end
+  
+      // Query to fetch all GORT-5 items for the given student ID
+      const query = "SELECT * FROM gort WHERE student_id = $1";
+      const { rows } = await pool.query(query, [studentId]);
+  
+      // Sending the response with the GORT items
+      res.json(rows);
+    } catch (error) {
+      console.error("Error fetching GORT items:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+  module.exports = router;
 
 
-
-
-
-module.exports = router;
+                              // POST
+router.post("/:studentId", async (req, res) => {
+    try{
+        const studentId = req.params.studentId; // TODO: may just be id from front end
+        const query = ""
+    }
+})

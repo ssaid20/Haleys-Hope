41e48754b14b135dd91b615076327f205fd9d99f
Said Said <ssaid20@icloud.com>
2023-11-11 22:06:11 -0600

    Add sagas for updating and deleting younger C-TOPP records

--- src/redux/sagas/youngerCtopp.saga.js
+++ src/redux/sagas/youngerCtopp.saga.js
@@ -12,7 +12,6 @@ function* fetchYoungerCtopp(action) {
     console.log("Error fetching younger_ctopp data", error);
   }
 }
-
 // Saga for adding younger_ctopp data
 function* addYoungerCtopp(action) {
   try {
@@ -25,11 +24,45 @@ function* addYoungerCtopp(action) {
     console.log("Error adding younger_ctopp data", error);
   }
 }
+// saga for updating younger ctopp need student id as well as test id
+function* updateYoungerCtopp(action) {
+  try {
+    yield call(
+      axios.put,
+      `/api/younger_ctopp/${action.payload.student_id}/${action.payload.id}`,
+      action.payload
+    );
+    yield put({
+      type: "FETCH_YOUNGER_CTOPP",
+      payload: action.payload.student_id,
+    });
+  } catch (error) {
+    console.log("Error updating younger ctopp data", error);
+  }
+}
+
+// saga for deleting an individual ctopp test for a student. needs student id and test id
+function* deleteYoungerCtopp(action) {
+  try {
+    yield call(
+      axios.delete,
+      `/api/younger_ctopp/${action.payload.student_id}/${action.payload.id}`
+    );
+    yield put({
+      type: "FETCH_YOUNGER_CTOPP",
+      payload: action.payload.student_id,
+    });
+  } catch (error) {
+    console.log("Error deleting younger ctopp data", error);
+  }
+}
 
 // watcher saga
 function* youngerCtoppSaga() {
   yield takeLatest("FETCH_YOUNGER_CTOPP", fetchYoungerCtopp);
   yield takeLatest("ADD_YOUNGER_CTOPP", addYoungerCtopp);
+  yield takeLatest("UPDATE_YOUNGER_CTOPP", updateYoungerCtopp);
+  yield takeLatest("DELETE_YOUNGER_CTOPP", deleteYoungerCtopp);
 }
 
 export default youngerCtoppSaga;

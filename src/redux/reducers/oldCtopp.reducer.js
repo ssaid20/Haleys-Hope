9053a8fa863e234aaddb323b42e362292354ed7f
Said Said <ssaid20@icloud.com>
2023-11-11 20:17:53 -0600

    added older_ctopp test reducer

--- src/redux/reducers/oldCtopp.reducer.js
+++ src/redux/reducers/oldCtopp.reducer.js
@@ -1,1 +1,38 @@
+const initialState = {
+  list: [], // Array to store the list of older ctopp records
+  isLoading: false, // To track the loading state of requests
+  error: null, // To store any errors that might occur during requests
+  currentRecord: null, // Object to store details of the currently selected older ctopp record
+};
 
+// defining the olderCtoppReducer with the initial state
+const olderCtoppReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_OLDER_CTOPP":
+      return {
+        ...state,
+        list: action.payload,
+      };
+    case "ADD_OLDER_CTOPP":
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+      };
+    case "UPDATE_OLDER_CTOPP":
+      return {
+        ...state,
+        list: state.list.map((item) =>
+          item.id === action.payload.id ? action.payload : item
+        ),
+      };
+    case "DELETE_OLDER_CTOPP":
+      return {
+        ...state,
+        list: state.list.filter((item) => item.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+export default olderCtoppReducer;

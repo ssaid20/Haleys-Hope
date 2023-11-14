fdf15081b7eebd2553ec83572955efb27387df8e
David Campbell <tryggus20@gmail.com>
2023-11-14 16:37:29 -0600

    added assessment components

---
+++ src/components/AssessmentResults/AssessmentResults.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { formatDate } from "../../lib/utils";
+import { useParams } from "react-router-dom/";
+
+
+const AssessmentResults = () => {
+
+    // map through all of the tests and based on date add to a new object. then 
+    return (
+      <div>
+        <p style={{ color: 'brown' }}>Hello Assessments
+        </p>
+
+        {/* <AssessmentGort />
+        <AssessmentWistE />  TODO: Add components as cards for each test based on date
+        <AssessmentWistS />
+        <AssessmentCtoppE />
+        <AssessmentCtoppS />
+         */}
+
+
+      </div>
+    )}
+
+
+
+
+export default AssessmentResults;

55eea7815a604e3614cd97c51abdb652e470a1db
David Campbell <tryggus20@gmail.com>
2023-11-17 11:00:08 -0600

    mapping over assessments is working

--- src/components/AssessmentResults/AssessmentResults.jsx
+++ src/components/AssessmentResults/AssessmentResults.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState, useHistory} from "react";
+import React, { useEffect, useState} from "react";
 import { useSelector, useDispatch  } from "react-redux";
 import { formatDate } from "../../lib/utils";
-import { useParams } from "react-router-dom/";
+import { useParams, useHistory } from "react-router-dom/";
 
 
 const AssessmentResults = () => {
-    const store = useSelector((store) => store);
-    console.log("assessment STORE",store);
+    const olderAssessment = useSelector((store) => store.assessmentReducer.olderAssessment);
+    const youngerAssessment = useSelector((store) => store.assessmentReducer.youngerAssessment)
+    const history = useHistory();
+
+    console.log("younger assessment ",youngerAssessment);
+    console.log("older assessment ",olderAssessment);
 
     const studentId = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
     console.log("theStudentId ISSSS", studentId.id);
-    dispatch({ type: "FETCH_YOUNGER_ASSESSMENT", payload: studentId.id });
-    dispatch({ type: "FETCH_OLDER_ASSESSMENT", payload: studentId.id });
-
   }, []);
 
-//   const moreDetails = (testId) => {
-// history.push(`/AssessmentResults/${testId}`);
-// };
+  const moreDetails = (testId) => {
+history.push(`/AssessmentResults/${testId}`);
+};
   // map through all of the tests and based on date add to a new object. then 
   // pass each test.id to component to render that section of the assessment?
     return (
@@ -28,14 +29,20 @@ const AssessmentResults = () => {
         </p>
 
     <div>
-      {/* {.map((test) => (
+      {olderAssessment.map((test) => (
         <div key={test.id} onClick={() => moreDetails(test.id)}>
-          <p>Wist Age 7-11 Date: {formatDate(test.date)} (click for details)</p>
+          <p>Assessment Date: {formatDate(test.date)} (click for details)</p>
         </div>
-      ))} */}
+      ))}
     </div>
-  {/* ); */}
-{/* }; */}
+    <div>
+      {youngerAssessment.map((test) => (
+        <div key={test.id} onClick={() => moreDetails(test.id)}>
+          <p>Assessment: {formatDate(test.date)} (click for details)</p>
+        </div>
+      ))}
+    </div>
+ 
 
         {/* <AssessmentGort />
         <AssessmentWistE />  TODO: Add components as cards for each test based on date

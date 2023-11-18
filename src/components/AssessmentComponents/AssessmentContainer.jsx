c01dffc342296a62e1f80d9b200fb64b0a538c4b
David Campbell <tryggus20@gmail.com>
2023-11-18 12:00:59 -0600

    gort assessment functionality done

--- src/components/AssessmentComponents/AssessmentContainer.jsx
+++ src/components/AssessmentComponents/AssessmentContainer.jsx
@@ -7,7 +7,8 @@ import AssessmentCtoppE from "./AssessmentCtoppE";
 import AssessmentCtoppS from "./AssessmentCtoppS";
 import AssessmentWistE from "./AssessmentWistE";
 import AssessmentWistS from "./AssessmentWistS";
-
+// TODO: CHANGE ROUTE FOR GET TO DO EACH TEST SEPARATELY TO 
+// MAKE SURE IF ONE DOES NOT EXIST, IT WILL STILL WORK
 
 const AssessmentContainer = () => {
   return (

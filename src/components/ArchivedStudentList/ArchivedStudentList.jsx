fd789c6ad4527d9747aa45c8208764e9d6e0ab84
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-11-20 08:16:10 -0600

    added some styling to the coaches list

--- src/components/ArchivedStudentList/ArchivedStudentList.jsx
+++ src/components/ArchivedStudentList/ArchivedStudentList.jsx
@@ -45,78 +45,81 @@ const ArchivedStudentList = () => {
   };
   console.log("logging archived Students in list", students);
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 840 }}>
-        <Table stickyHeader aria-label="student table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {students
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((student) => {
-                const formattedStudent = {
-                  ...student,
-                  name: `${student.first_name} ${student.last_name}`,
-                  age: calculateAge(student.dob),
-                  city: student.city, // *** need to fix our address input then make function to pull out city and state ***
-                  //on_site: student.on_site ? "On-Site" : "Virtual", *** only need on more details ***
-                  // dob: formatDate(student.dob), // formatting date *** only need on more details ***
-                  start_date: formatDate(student.barton_c_date), // *** using pretest date, do we need a start date column? ***
-                  picture: (
-                    <img
-                      src={student.picture}
-                      alt={`${student.first_name} ${student.last_name}`}
-                      style={{ width: "50px", height: "50px" }}
-                    />
-                  ),
-                };
-
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={student.id}
-                    onClick={() =>
-                      history.push(`/students/archived-student/${student.id}`)
-                    }
-                    style={{ cursor: "pointer" }}
+    <>
+      <h1>Archived Students</h1>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 840 }}>
+          <Table stickyHeader aria-label="student table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
                   >
-                    {columns.map((column) => {
-                      const value = formattedStudent[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={students.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {students
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((student) => {
+                  const formattedStudent = {
+                    ...student,
+                    name: `${student.first_name} ${student.last_name}`,
+                    age: calculateAge(student.dob),
+                    city: student.city, // *** need to fix our address input then make function to pull out city and state ***
+                    //on_site: student.on_site ? "On-Site" : "Virtual", *** only need on more details ***
+                    // dob: formatDate(student.dob), // formatting date *** only need on more details ***
+                    start_date: formatDate(student.barton_c_date), // *** using pretest date, do we need a start date column? ***
+                    picture: (
+                      <img
+                        src={student.picture}
+                        alt={`${student.first_name} ${student.last_name}`}
+                        style={{ width: "50px", height: "50px" }}
+                      />
+                    ),
+                  };
+
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={student.id}
+                      onClick={() =>
+                        history.push(`/students/archived-student/${student.id}`)
+                      }
+                      style={{ cursor: "pointer" }}
+                    >
+                      {columns.map((column) => {
+                        const value = formattedStudent[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={students.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </>
   );
 };
 

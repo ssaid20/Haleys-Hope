d4d440dda15d52739a6978b46197b8614d105b98
Said Said <ssaid20@icloud.com>
2023-11-21 16:26:18 -0600

    added a new tab for test comparison

---
+++ src/components/shared/CompareTab/CompareTab.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GortComparisonTable from "../../TestComparisons/GortComparison";
+
+const CompareTab = () => {
+  return (
+    <div>
+      <div className="h2-bold">GORT-5 TESTS COMPARISON</div>
+
+      <GortComparisonTable />
+    </div>
+  );
+};
+
+export default CompareTab;

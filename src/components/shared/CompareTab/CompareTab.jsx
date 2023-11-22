863c99cf8a157ba23cf860fadd5810fd8d8ed311
David Campbell <tryggus20@gmail.com>
2023-11-22 08:53:05 -0600

    imported compare results to correct page

--- src/components/shared/CompareTab/CompareTab.jsx
+++ src/components/shared/CompareTab/CompareTab.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import GortComparisonTable from "../../TestComparisons/GortComparison";
+import SecondaryWistComparisonTable from "../../TestComparisons/SecondaryWistComparison";
+import ElementaryWistComparisonTable from "../../TestComparisons/ElementaryWistComparison";
 
 const CompareTab = () => {
   return (
     <div>
-      <div className="h2-bold">GORT-5 TESTS COMPARISON</div>
-
+      <div className="h2-bold">GORT-5 TEST COMPARISON</div>
       <GortComparisonTable />
+
+      <div className="h2-bold">WIST 11-18 TEST COMPARISON</div>
+      <SecondaryWistComparisonTable />
+     
+      <div className="h2-bold">WIST 7-11 TEST COMPARISON</div>
+      <ElementaryWistComparisonTable />
     </div>
   );
 };

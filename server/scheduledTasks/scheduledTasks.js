f3a88d6a8ce8a7c70dd6a47efdd25310d9e0bfe2
David Campbell <tryggus20@gmail.com>
2023-11-23 11:04:49 -0600

    cron seems to be workng

--- server/scheduledTasks/scheduledTasks.js
+++ server/scheduledTasks/scheduledTasks.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { incrementGrades } = require('../routes/gradeService');
 
 const setupCronJobs = () => {
-  cron.schedule('*/1 * * * *', () => {
+  cron.schedule('0 0 1 8 *', () => {
     console.log('Running grade increment job');
     incrementGrades();
   });

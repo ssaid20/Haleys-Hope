d50aba2ff88fc27b1063b158c9db2cb1e5305ce7
David Campbell <tryggus20@gmail.com>
2024-03-13 14:07:09 -0500

    fixing testTab component layout

--- src/components/shared/TestTab/TestTab.jsx
+++ src/components/shared/TestTab/TestTab.jsx
@@ -21,6 +21,70 @@ const TestTab = () => {
   return (
     <div>
       <Table>
+        <TableHead>
+           
+        </TableHead>
+        <TableHead align="center">
+          <TableRow>
+          <TableCell>
+    Test
+</TableCell>
+<TableCell>
+Date
+</TableCell>
+<TableCell>
+Date
+</TableCell>
+<TableCell>
+Date
+</TableCell>
+          </TableRow>
+        
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              Wist 7-11
+            </TableCell>
+            <TableCell>10-10-2010</TableCell>
+
+            <TableCell>11-11-2011</TableCell>
+            <TableCell>12-12-2012</TableCell>
+
+
+            <TableCell>
+
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+Wist 11-18         </TableCell>
+<TableCell>11-11-2011</TableCell>
+<TableCell>11-11-2011</TableCell>
+<TableCell>11-11-2011</TableCell>
+
+          </TableRow>         <TableRow>
+            <TableCell>
+Ctopp 4-6        </TableCell>
+<TableCell>11-11-2011</TableCell>
+<TableCell>11-11-2011</TableCell>
+<TableCell>11-11-2011</TableCell>
+
+          </TableRow>
+          <TableRow>
+            <TableCell>
+Ctopp 7-24         </TableCell>
+<TableCell>11-11-2011</TableCell>
+<TableCell>11-11-2011</TableCell>
+<TableCell>11-11-2011</TableCell>
+
+          </TableRow>
+
+        </TableBody>
+      </Table>
+
+      <Table>
+      
       <ElementaryWistList />
       <SecondaryWistList />
       <GortList />

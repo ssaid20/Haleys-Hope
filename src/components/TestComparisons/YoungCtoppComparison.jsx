d41bbe0e5f8bd556032709e7438ded084fe3c6a8
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2024-03-16 14:09:47 -0500

    younger ctopp descriptors working

--- src/components/TestComparisons/YoungCtoppComparison.jsx
+++ src/components/TestComparisons/YoungCtoppComparison.jsx
@@ -43,28 +43,36 @@ function createRowData(category, tests) {
     "Phonological Awareness": {
       percentile: "phonological_awareness_percentile",
       scaled: "phonological_awareness_composite",
+      descriptor: "phonological_awareness_descriptor",
     },
     "Phonological Memory": {
       percentile: "phonological_memory_percentile",
       scaled: "phonological_memory_composite",
+      descriptor: "phonological_memory_descriptor",
     },
     "Rapid Symbolic Naming": {
       percentile: "rapid_symbolic_naming_percentile",
       scaled: "rapid_symbolic_naming_composite",
+      descriptor: "rapid_symbolic_naming_descriptor",
     },
     "Rapid Non-Symbolic Naming": {
       percentile: "rapid_non_symbolic_naming_percentile",
       scaled: "rapid_non_symbolic_naming_composite",
+      descriptor: "rapid_non_symbolic_naming_descriptor",
     },
   };
 
   return {
     category,
     percentiles: tests.map((test) => test[categoryMap[category].percentile]),
-    scaledScores: tests.map((test) => test[categoryMap[category].scaled]),
+    scaledScores: tests.map((test) => ({
+      score: test[categoryMap[category].scaled],
+      descriptor: test[categoryMap[category].descriptor], // This assumes descriptor is directly available
+    })),
     descriptiveTerms: tests.map((test) =>
       GetCompositeScoreDescription({
         compositeScore: test[categoryMap[category].scaled],
+        descriptor: test[categoryMap[category].descriptor],
       })
     ), // Assuming you calculate descriptive terms based on scaled scores
   };
@@ -99,80 +107,83 @@ const YoungCtoppComparison = () => {
   const lightGreyColor = "#F5F5F5"; // Light grey color
   if (youngerCtoppTests.length === 0) {
     return (
-    <div><p>No CTOPP 4-16 Assessments for this student </p></div>)
-  
-  }
-  else if (youngerCtoppTests.length === 1){
-    return( <div><p>Only 1 CTOPP Test 4-16 exists  </p></div>)
- 
-   }
-  else {
-  return (
-    <>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700}} aria-label="GORT-5 Comparison Table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell color={lightGreyColor}>Category</StyledTableCell>
-            {youngerCtoppTests.map((test, index) => (
-              <TestHeaderCell
-                align="center"
-                color={sectionHeaderColors.percentile}
-                key={`percentile-header-${index}`}
-              >
-                {`Test ${index + 1} (${formatDate3(test.date)}) Percentile`}
-              </TestHeaderCell>
-            ))}
-            {youngerCtoppTests.map((test, index) => (
-              <TestHeaderCell
-                align="center"
-                color={sectionHeaderColors.scaledScore}
-                key={`scaled-score-header-${index}`}
-              >
-                {`Test ${index + 1} (${formatDate3(test.date)}) Scaled Score`}
-              </TestHeaderCell>
-            ))}
-            {youngerCtoppTests.map((test, index) => (
-              <TestHeaderCell
-                align="center"
-                color={sectionHeaderColors.descriptiveTerm}
-                key={`descriptive-term-header-${index}`}
-              >
-                {`Test ${index + 1} (${formatDate3(test.date)}) Descriptive`}
-              </TestHeaderCell>
-            ))}
-          </TableRow>
-        </TableHead>
+      <div>
+        <p>No CTOPP 4-16 Assessments for this student </p>
+      </div>
+    );
+  } else if (youngerCtoppTests.length === 1) {
+    return (
+      <div>
+        <p>Only 1 CTOPP Test 4-16 exists </p>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="GORT-5 Comparison Table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell color={lightGreyColor}>Category</StyledTableCell>
+                {youngerCtoppTests.map((test, index) => (
+                  <TestHeaderCell
+                    align="center"
+                    color={sectionHeaderColors.percentile}
+                    key={`percentile-header-${index}`}
+                  >
+                    {`Test ${index + 1} (${formatDate3(test.date)}) Percentile`}
+                  </TestHeaderCell>
+                ))}
+                {youngerCtoppTests.map((test, index) => (
+                  <TestHeaderCell
+                    align="center"
+                    color={sectionHeaderColors.scaledScore}
+                    key={`scaled-score-header-${index}`}
+                  >
+                    {`Test ${index + 1} (${formatDate3(test.date)}) Scaled Score`}
+                  </TestHeaderCell>
+                ))}
+                {youngerCtoppTests.map((test, index) => (
+                  <TestHeaderCell
+                    align="center"
+                    color={sectionHeaderColors.descriptiveTerm}
+                    key={`descriptive-term-header-${index}`}
+                  >
+                    {`Test ${index + 1} (${formatDate3(test.date)}) Descriptive`}
+                  </TestHeaderCell>
+                ))}
+              </TableRow>
+            </TableHead>
 
-        <TableBody>
-          {rows.map((row, rowIndex) => (
-            <StyledTableRow key={rowIndex}>
-              <StyledTableCell component="th" scope="row">
-                {row.category}
-              </StyledTableCell>
-              {row.percentiles.map((percentile, index) => (
-                <StyledTableCell key={`percentile-${index}`} align="right">
-                  {percentile}
-                </StyledTableCell>
+            <TableBody>
+              {rows.map((row, rowIndex) => (
+                <StyledTableRow key={rowIndex}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.category}
+                  </StyledTableCell>
+                  {row.percentiles.map((percentile, index) => (
+                    <StyledTableCell key={`percentile-${index}`} align="right">
+                      {percentile}
+                    </StyledTableCell>
+                  ))}
+                  {row.scaledScores.map((scoreObj, index) => (
+                    <StyledTableCell key={`score-${index}`} align="right">
+                      {scoreObj.descriptor ? `${scoreObj.descriptor}${scoreObj.score}` : scoreObj.score}
+                    </StyledTableCell>
+                  ))}
+                  {row.descriptiveTerms.map((term, index) => (
+                    <StyledTableCell key={`descriptive-${index}`} align="right">
+                      {term}
+                    </StyledTableCell>
+                  ))}
+                </StyledTableRow>
               ))}
-              {row.scaledScores.map((score, index) => (
-                <StyledTableCell key={`score-${index}`} align="right">
-                  {score}
-                </StyledTableCell>
-              ))}
-              {row.descriptiveTerms.map((term, index) => (
-                <StyledTableCell key={`descriptive-${index}`} align="right">
-                  {term}
-                </StyledTableCell>
-              ))}
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <div style={{ breakAfter: 'page' }}></div>
-</>
-  );
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div style={{ breakAfter: "page" }}></div>
+      </>
+    );
+  }
 };
-}
 export default YoungCtoppComparison;
